6272bd59a97f32592dd81d3e33db10562e066a3a
Terje Urnes <terje@urnes.net>
2024-01-14 05:48:50 +0100

    Adding block to leaf.

---
+++ b-plus-src/node.js
@@ -0,0 +1,141 @@
+
+class Node {
+
+    #type;
+    #orderOfTheTree;
+    #objectCount = 0;
+    #objectArray
+    #keyArray
+    
+
+    get type() {
+        return this.#type;
+    }
+
+    get isInternal() {
+        return this.#type == NodeType.internal;
+    }
+
+    get isLeaf() {
+        return this.#type == NodeType.leaf;
+    }
+
+    get childCount() {
+        return this.#type == NodeType.internal ? this.#objectCount : 0;
+    }
+
+    get blockCount() {
+        return this.#type == NodeType.leaf ? this.#objectCount : 0;
+    }
+
+    get childArray() {
+        return this.#type == NodeType.internal ? this.#objectArray : undefined;
+    }
+
+    get blockArray() {
+        return this.#type == NodeType.leaf ? this.#objectArray : undefined;
+    }
+
+    get keyCount() {
+        if (this.#type == NodeType.leaf) {
+            return this.#objectCount;
+        }
+        const count = this.#objectCount - 1;
+        if (count == -1) {
+            return 0;
+        }
+        return count;
+    }
+
+    get keyArray() {
+        return this.#keyArray;
+    }
+
+    constructor(type, orderOfTheTree, underflow = Math.ceil(orderOfTheTree/2), overflow = Math.ceil(orderOfTheTree/2)) {
+        this.#testType(type);
+        this.#testOrderOfTheTree(orderOfTheTree);
+        this.#type = type;
+        this.#orderOfTheTree = orderOfTheTree;
+        this.#initArrays();
+    }
+
+    #testOrderOfTheTree(orderOfTheTree) {
+        if (isNaN(orderOfTheTree) || orderOfTheTree < 2) {
+            throw new Error("The order of the tree has to be 2 or greater.");
+        }
+    }
+
+    #testType(type) {
+        if ((type == NodeType.internal || type == NodeType.leaf) == false) {
+            throw new Error("A node have to be ether of type internal or leaf");
+        }
+    }
+
+    #initArrays() {
+        if (this.#type == NodeType.internal) {
+            this.#objectArray = new Array(this.#orderOfTheTree + 1);
+        }
+        else {
+            this.#objectArray = new Array(this.#orderOfTheTree);
+        }
+        this.#keyArray = new Array(this.#orderOfTheTree);
+    }
+
+    addBlock(block) {
+        if (this.isLeaf) {
+            this.#addBlock(block);
+        }
+        else {
+            const index = this.#findKeyIndex(block.blockKey);
+            this.#objectArray[index].addBlock(block);
+        }
+    }
+
+    #addBlock(block) {
+        const index = this.#findKeyIndex(block.blockKey)
+        this.#throwErrorIfKeyIsNotUnique(block.blockKey, index);
+        this.#placeKeyInArray(block.blockKey, index);
+        this.#placeBlockInArray(block, index);
+        this.#objectCount++;
+    }
+
+    #findKeyIndex(key) {
+        const keyCount = this.keyCount;
+        for (let i = 0; i < keyCount; i++) {
+            if (key <= this.#keyArray[i]) {
+                return i;
+            }
+        }
+        return keyCount;
+    }
+
+    #throwErrorIfKeyIsNotUnique(key, index) {
+        if (this.#objectArray[index] != undefined && this.#objectArray[index].blockKey == key) {
+            throw new Error("Key has to be unique.");
+        }
+    }
+
+    #placeKeyInArray(key, index) {
+        this.#placeInArray(this.#keyArray, key, index);
+    }
+
+    #placeBlockInArray(block, index) {
+        this.#placeInArray(this.#objectArray, block, index);
+    }
+
+    #placeInArray(array, element, index) {
+        for (let i = this.#objectCount-1; i >= index; i--) {
+            array[i + 1] = array[i];
+        }
+        array[index] = element;
+        console.log(element);
+    }
+
+}
+
+const NodeType = {
+    internal: "nodeType-internal",
+    leaf: "nodeType-leaf"
+};
+
+export { Node, NodeType };
---
+++ tests/node-basic.test.js
@@ -0,0 +1,138 @@
+import { LeafBlock } from "../b-plus-src/LeafBlock.js";
+import { Node, NodeType } from "../b-plus-src/node.js";
+
+var instanceA;
+var instanceB;
+beforeEach( () => {
+    instanceA = new Node(NodeType.internal, 2);
+    instanceB = new Node(NodeType.leaf, 2);
+});
+
+test("Can initiate instance", () => {
+    expect(instanceA).toBeDefined();
+    expect(instanceB).toBeDefined();
+});
+
+test("An internal node has type internal", () => {
+    expect(instanceA.type).toMatch(NodeType.internal);
+});
+
+test("A leaf node has type leaf", () => {
+    expect(instanceB.type).toMatch(NodeType.leaf);
+});
+
+test("A empty string as type throws an exceptions", () => {
+    expect(() => {new Node("");}).toThrow("A node have to be ether of type internal or leaf");
+});
+
+test("The 'enum' as type throws an exceptions", () => {
+    expect(() => {new Node(NodeType);}).toThrow("A node have to be ether of type internal or leaf");
+});
+
+test("A string with 'internal' or 'leaf' as type throws an exceptions", () => {
+    expect(() => {new Node("internal");}).toThrow("A node have to be ether of type internal or leaf");
+    expect(() => {new Node("leaf");}).toThrow("A node have to be ether of type internal or leaf");
+});
+
+test("A number as type throws an exceptions", () => {
+    expect(() => {new Node(42);}).toThrow("A node have to be ether of type internal or leaf");
+});
+
+test("An internal node returns true from property isInternal", () => {
+    expect(instanceA.isInternal).toBeTruthy();
+});
+
+test("An leaf node returns false from property isInternal", () => {
+    expect(instanceB.isInternal).toBeFalsy();
+});
+
+test("An leaf node returns true from property isLeaf", () => {
+    expect(instanceB.isLeaf).toBeTruthy();
+});
+
+test("An internal node returns false from property isLeaf", () => {
+    expect(instanceA.isLeaf).toBeFalsy();
+});
+
+test("A new internal node has zero child's.", () => {
+    expect(instanceA.childCount).toBe(0);
+});
+
+test("A new internal node has zero blocks.", () => {
+    expect(instanceA.blockCount).toBe(0);
+});
+
+test("A new leaf node has zero child's.", () => {
+    expect(instanceB.childCount).toBe(0);
+});
+
+test("A new leaf node has zero blocks.", () => {
+    expect(instanceB.blockCount).toBe(0);
+});
+
+test("A internal node has a child array", () => {
+    expect(instanceA.childArray).toBeDefined();
+});
+
+test("A leaf node has not a child array", () => {
+    expect(instanceB.childArray).toBeUndefined();
+});
+
+test("A internal node has not a block array", () => {
+    expect(instanceA.blockArray).toBeUndefined();
+});
+
+test("A leaf node has a block array", () => {
+    expect(instanceB.blockArray).toBeDefined();
+});
+
+test("A internal node of order 10 has has a 11 size child array", () => {
+    const instanceC = new Node(NodeType.internal, 10);
+    expect(instanceC.childArray.length).toBe(11);
+})
+
+test("A leaf node of order 10 has has a 10 size block array", () => {
+    const instanceC = new Node(NodeType.leaf, 10);
+    expect(instanceC.blockArray.length).toBe(10);
+})
+
+test("A new node has key count equal to zero", () => {
+    expect(instanceA.keyCount).toBe(0);
+    expect(instanceB.keyCount).toBe(0);
+});
+
+test("A new node has zero keys", () => {
+    expect(instanceA.keyArray).toEqual([, ]);
+    expect(instanceB.keyArray).toEqual([, ]);
+})
+
+test("Adding a block increases block count.", () => {
+    const elm = [{key:1}];
+    instanceB.addBlock(new LeafBlock(elm));
+    expect(instanceB.blockCount).toBe(1);
+});
+
+test("Adding a block populates block array", () => {
+    const elm = [{key:1}];
+    const block = new LeafBlock(elm)
+    instanceB.addBlock(block);
+    expect(instanceB.blockArray[0]).toStrictEqual(block);
+});
+
+test("Adding two blocks gives a block count of two.", () => {
+    const elm1 = [{key:1}];
+    const elm2 = [{key:2}];
+    instanceB.addBlock(new LeafBlock(elm1));
+    instanceB.addBlock(new LeafBlock(elm2));
+    expect(instanceB.blockCount).toBe(2);
+});
+
+test("Adding two blocks with identical keys throws an exception", () => {
+    const elm1 = [{key:1}];
+    const elm2 = [{key:1}];
+    instanceB.addBlock(new LeafBlock(elm1));
+    expect(() => {instanceB.addBlock(new LeafBlock(elm2));}).toThrow("Key has to be unique.");
+})
+
+
+
